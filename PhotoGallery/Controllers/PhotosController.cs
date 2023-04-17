0c349854e52e0db04f3d61f4c966a0dc1ec4dc42
Ramadan Cilomba <rcilomba@yahoo.com>
2023-04-17 22:16:12 +0200

    photos.json almost done

---
+++ PhotoGallery/Controllers/PhotosController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoGallery.Models;
+using System.Text.Json;
+
+namespace PhotoGallery.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private readonly ILogger<PhotosController> _logger;
+        private readonly IConfiguration _config;
+
+        public PhotosController(ILogger<PhotosController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            var photos = GetPhotosFromJsonFile();
+
+            return View(photos);
+        }
+
+        public IActionResult Details(string id)
+        {
+            var photo = GetPhotoByIdFromJsonFile(id);
+
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", photo.Url.TrimStart('/'));
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            return new FileStreamResult(fileStream, "image/jpeg");
+        }
+
+        private List<Photos> GetPhotosFromJsonFile()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "db.json");
+            var json = System.IO.File.ReadAllText(filePath);
+            var photos = JsonSerializer.Deserialize<Photos>(json);
+            return photos.PhotoList;
+        }
+
+        private Photos GetPhotoByIdFromJsonFile(string id)
+        {
+            var photos = GetPhotosFromJsonFile();
+            var photo = photos.FirstOrDefault(p => p.Id == id);
+
+            return photo;
+        }
+    }
+
+}
